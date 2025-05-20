a0069dcb11c1301787eff1df94194933c99432c6
nolaram <marloncopino10@gmail.com>
2025-05-20 13:55:15 +0800

    initialize_label

--- input_box.py
+++ input_box.py
@@ -2,7 +2,8 @@ import pygame
 
 class InputBox:
     # method initialize
-    def __init__(self, x, y, w, h):
+    def __init__(self, x, y, w, h, label):
         self.rect = pygame.Rect(x, y, w, h)
+        self.label = label
     # method handle event
     # method draw
