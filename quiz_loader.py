e82f8992ea59fbfe82646f6915945494c9c9c0b4
nolaram <marloncopino10@gmail.com>
2025-05-20 18:40:40 +0800

    load_questions

--- quiz_loader.py
+++ quiz_loader.py
@@ -1,7 +1,11 @@
 # import the question file
 from question import Question
 # class
+class QuizLoader:
+    @staticmethod
     # load the questions
+    def load_questions_from_file(file_path):
+        questions = []
     # initialize a list
     # open the file
     # loop through each line
