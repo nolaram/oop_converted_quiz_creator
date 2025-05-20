024b811d730bae7fa36791749946e28778aab1ca
nolaram <marloncopino10@gmail.com>
2025-05-20 13:57:18 +0800

    initialize_index

--- input_box.py
+++ input_box.py
@@ -2,10 +2,11 @@ import pygame
 
 class InputBox:
     # method initialize
-    def __init__(self, x, y, w, h, label, font):
+    def __init__(self, x, y, w, h, label, font, index):
         self.rect = pygame.Rect(x, y, w, h)
         self.label = label
         self.text = ""
         self.font = font
+        self.index = index
     # method handle event
     # method draw
