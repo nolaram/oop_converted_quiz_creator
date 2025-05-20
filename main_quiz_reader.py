e09887abf3fe295207db64b47c053db8c608b9cd
nolaram <marloncopino10@gmail.com>
2025-05-20 19:53:26 +0800

    if_no_read_file_exit

--- main_quiz_reader.py
+++ main_quiz_reader.py
@@ -8,7 +8,11 @@ if __name__ == "__main__":
     # read file
     file_name = "quiz_data.txt"
     questions = QuizLoader.load_questions_from_file(file_name)
-    
+
     # if no read file
+    if not questions:
+        print("No questions loaded. Exiting.")
         # exit
+        sys.exit()
+        
     # start quiz
