c1ca77fddd3fe5ca1e64e17df764411f23075bd8
nolaram <marloncopino10@gmail.com>
2025-05-20 13:54:10 +0800

    make_class_and_initialize

--- input_box.py
+++ input_box.py
@@ -1,6 +1,8 @@
 import pygame
 
-# class
+class InputBox:
     # method initialize
+    def __init__(self, x, y, w, h):
+        self.rect = pygame.Rect(x, y, w, h)
     # method handle event
     # method draw
