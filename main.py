58c9b9aaca0d14d8eb10b16d19e62a730955a025
nolaram <marloncopino10@gmail.com>
2025-05-20 14:56:37 +0800

    make_the_screen

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ import sys
 from input_box import InputBox
 from quiz_data import QuizDataWriter
 
-# class
+class QuizCreatorApp:
     # constructor method
+    def __init__(self):
         # screen
+        pygame.init()
+        self.WIDTH, self.HEIGHT = 800, 600
+        self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+        pygame.display.set_caption('Quiz Creator')
+        self.clock = pygame.time.Clock()
+        
     # run the screen
 # call main
