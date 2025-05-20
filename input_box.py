ff3318ba485179baae2eb0f5970569b6f7c8a6f9
nolaram <marloncopino10@gmail.com>
2025-05-20 13:56:39 +0800

    initialize_font

--- input_box.py
+++ input_box.py
@@ -2,9 +2,10 @@ import pygame
 
 class InputBox:
     # method initialize
-    def __init__(self, x, y, w, h, label):
+    def __init__(self, x, y, w, h, label, font):
         self.rect = pygame.Rect(x, y, w, h)
         self.label = label
         self.text = ""
+        self.font = font
     # method handle event
     # method draw
