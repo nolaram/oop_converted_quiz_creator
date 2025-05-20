e5c5c5aada35e762242c1a0279dbb40489b6c3ce
nolaram <marloncopino10@gmail.com>
2025-05-20 19:14:01 +0800

    pseudocode_for_quiz

---
+++ quiz.py
@@ -0,0 +1,14 @@
+# import all things needed
+# class
+    # construct method
+        # question
+        # time limit
+        # score
+    # main function to start the program
+        # loop 
+        # display results
+        # ask to retry
+    # handle display
+        # show the question
+        # display options
+        # timer
