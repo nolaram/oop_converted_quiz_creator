2bd3413a7c569d331642c0698c65488273e407fb
nolaram <marloncopino10@gmail.com>
2025-05-20 18:26:40 +0800

    store_correct_answer_in_lower_case

--- question.py
+++ question.py
@@ -1,6 +1,6 @@
 class Question:
     # constructor method
-    def __init__(self, question_text, option_a, option_b, option_c, option_d):
+    def __init__(self, question_text, option_a, option_b, option_c, option_d, correct_answer):
         self.question_text = question_text
         # parameters
         self.options = {
@@ -9,4 +9,6 @@ class Question:
             'c': option_c,
             'd': option_d
         }
+        self.correct_answer = correct_answer.lower()
+        
     # check if correct
