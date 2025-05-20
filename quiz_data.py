1f8e426c0197d95b21a9e9b308517eedc8218362
nolaram <marloncopino10@gmail.com>
2025-05-20 14:35:12 +0800

    write_questions_and_answer_to_file

--- quiz_data.py
+++ quiz_data.py
@@ -3,5 +3,14 @@ class QuizDataWriter:
     def __init__(self, filename):    
         # file name
         self.filename = filename
-        
+
     # save method
+    def save(self, inputs):
+        with open(self.filename, 'a') as file:
+            file.write(f'Question: {inputs[0]}\n')
+            file.write(f'a) {inputs[1]}\n')
+            file.write(f'b) {inputs[2]}\n')
+            file.write(f'c) {inputs[3]}\n')
+            file.write(f'd) {inputs[4]}\n')
+            file.write(f'Correct Answer: {inputs[5].lower()}\n')
+            file.write('-' * 40 + '\n')
