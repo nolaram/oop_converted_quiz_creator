00ad8b1b93775a8c8afa4e6876fd7185fcf0a060
nolaram <marloncopino10@gmail.com>
2025-05-20 15:04:57 +0800

    run_the_screen

--- main.py
+++ main.py
@@ -13,6 +13,20 @@ class QuizCreatorApp:
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
         pygame.display.set_caption('Quiz Creator')
         self.clock = pygame.time.Clock()
-        
+
     # run the screen
+    def run(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            self.screen.fill((0, 0, 0))
+            pygame.display.flip()
+            self.clock.tick(30)
+
+        pygame.quit()
+        sys.exit()
+        
 # call main
