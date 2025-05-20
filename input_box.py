91d9f6f6d232fc748160e39bca330afee8e88006
nolaram <marloncopino10@gmail.com>
2025-05-20 15:21:53 +0800

    fix_tuple

--- input_box.py
+++ input_box.py
@@ -2,7 +2,7 @@ import pygame
 
 class InputBox:
     # method initialize
-    def __init__(self, x, y, w, h, label, font, index):
+    def __init__(self, x, y, w, h, label, font=None, index=0):
         self.rect = pygame.Rect(x, y, w, h)
         self.label = label
         self.text = ""
