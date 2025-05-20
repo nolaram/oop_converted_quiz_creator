df6906ee993dd086841184634889800a2f3ab5c0
nolaram <marloncopino10@gmail.com>
2025-05-20 13:56:01 +0800

    initialize_text

--- input_box.py
+++ input_box.py
@@ -5,5 +5,6 @@ class InputBox:
     def __init__(self, x, y, w, h, label):
         self.rect = pygame.Rect(x, y, w, h)
         self.label = label
+        self.text = ""
     # method handle event
     # method draw
