8e3df2818bb25089fec7c132a2c3adca90cb87e4
nolaram <marloncopino10@gmail.com>
2025-05-20 19:52:33 +0800

    call_main_and_read_file

--- main_quiz_reader.py
+++ main_quiz_reader.py
@@ -4,7 +4,11 @@ from quiz_loader import QuizLoader
 from quiz import Quiz
 
 # call main
+if __name__ == "__main__":
     # read file
+    file_name = "quiz_data.txt"
+    questions = QuizLoader.load_questions_from_file(file_name)
+    
     # if no read file
         # exit
     # start quiz
